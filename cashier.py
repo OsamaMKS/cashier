69756356bb7357e3c857b9a59e0d48fa1df1df0b
Osama Sider <osamamksider@hotmail.com>
2020-02-06 14:33:37 +0200

    Cashier application

--- cashier.py
+++ cashier.py
@@ -1,5 +1,28 @@
 def main():
 	# write code here
+	items = {"name":"","price":"","quantity":""}
+	items_list = []
+	while items['name'] != "done":
+		item = input("enter item name and 'done' when finished :")
+		if item == "done".lower():
+			break
+		else:
+			price = float(input("enter the price :"))
+			quantity = float(input("Enter the quantity :"))
+			items_list.append({"name":item, "price" : price, "quantity":quantity})
+
+	print()
+	print("-------------------")
+	print("receipt")
+	print("-------------------")
+	print()
+
+
+	for i in items_list:
+		print(str(i["quantity"]) +" " + i["name"] + " " +str(i["quantity"]*i["price"]))
+
+			#index +=1
+
 
 if __name__ == '__main__':
 	main()
